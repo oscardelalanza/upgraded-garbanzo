f66dc0b00d6da1617762fdac7619ad65264ae69d
oscardelalanza <oscardelalanza@gmail.com>
2022-02-23 21:49:39 -0800

    initial form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            console.log(values)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
