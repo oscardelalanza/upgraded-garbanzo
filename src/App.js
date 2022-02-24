e4976d67c19e30f5e351e0991e6051f6cc7b4435
oscardelalanza <oscardelalanza@gmail.com>
2022-02-23 22:01:55 -0800

    first api call

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ const App = () => {
         <Formik
           initialValues={{ search: '' }}
           onSubmit={async values => {
-            console.log(values)
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              { headers: {
+                'Authorization': 'Client-ID xhVtSm4YruJ-1iZT6DQRj-ANZbnspacbLQgrkewdg9g'
+                }})
+            const data = await response.json()
+            console.log(data)
           }}
         >
           <Form>
