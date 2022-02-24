5bed75e88786ec14de88f5deebd48013b24b105f
oscardelalanza <oscardelalanza@gmail.com>
2022-02-23 22:10:41 -0800

    rendering images

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+          <article key={photo.id} onClick={() => open(photo.links.html)}>
+            <img src={photo.urls.regular} alt=""/>
+            <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+          </article>)}
+        </div>
+      </div>
     </div>
   )
 }
